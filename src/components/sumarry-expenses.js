5257e287a9cce316b70b336d729afde8878b25a1
BuddhaL1T3 <jmriggs75@gmail.com>
2018-08-22 18:48:13 -0400

    got expenses to WORK!

--- src/components/sumarry-expenses.js
+++ src/components/sumarry-expenses.js
@@ -2,51 +2,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import { Link, Redirect } from 'react-router-dom';
 
-/* 
-pull the value of each amount due within the given month
-
-*/
-
 export class SummaryExpenses extends React.Component {
-  earliest(array) {
-    if (array.length > 1) {
-      array.reduce(function(pre, cur) {
-        return Date.parse(pre) > Date.parse(cur) ? cur : pre;
-      });
-    } else return array[0];
-  }
-
   render() {
-    let mostRecent,
-      unpaidBills,
-      dueDates,
-      earliestDate,
-      totalExpenses = 0;
-    if (this.props.accounts) {
-      this.props.accounts.forEach(account => {
-        unpaidBills = account.bills.filter(item => item.isPaid === false);
-        dueDates = unpaidBills.map(item => (item = item.dueDate));
-        earliestDate = this.earliest(dueDates);
-        mostRecent = unpaidBills.find(item => item.dueDate === earliestDate);
-        if (account.frequency === '6 Months') {
-          totalExpenses += mostRecent.amount / 6;
-        } else if (account.frequency === 'Annual') {
-          totalExpenses += mostRecent.amount / 12;
-        } else if (mostRecent.amount !== null) {
-          totalExpenses += mostRecent.amount;
-        }
-      });
-    }
+    let totalExpenses = 0;
 
-    // console.log(oneAccount);
-    console.log(this.props.accounts);
-    console.log(totalExpenses);
-    // console.log(totalExpenses);
+    this.props.accounts.forEach(account => {
+      const result = account.bills[account.bills.length - 1];
+      if (account.frequency === '6 Months') {
+        totalExpenses += result.amount / 6;
+      } else if (account.frequency === 'Annual') {
+        totalExpenses += result.amount / 12;
+      } else if (result.amount !== null) {
+        totalExpenses += result.amount;
+      }
+    });
+
+    const expenseAccounts = this.props.accounts.map((account, index) => {
+      const result = account.bills[account.bills.length - 1];
+      const AccFreq = { 'One Time': 1, Monthly: 1, '6 Months': 6, Annual: 12 };
+      return (
+        <li key={index}>
+          <p>
+            {account.name}
+            <span>{`${result.amount} / ${Math.round(
+              (result.amount / AccFreq[account.frequency] / totalExpenses) *
+                10000
+            ) / 100}%`}</span>
+          </p>
+        </li>
+      );
+    });
 
     return (
       <section className="summary-expenses">
-        <p>Expenses</p>
-        {/* {totalExpenses} */}
+        <p>
+          Expenses: <span>{totalExpenses}</span>
+        </p>
+        <ul>{expenseAccounts}</ul>
       </section>
     );
   }
