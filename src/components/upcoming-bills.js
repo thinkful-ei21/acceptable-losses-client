c43a1881127c869200700467d6eafa3f7a109a56
Courtney Adams <cadams8238@gmail.com>
2018-08-22 12:05:07 -0700

    populating dashboard with upcoming bills. Right now ALL upcoming bills are shown. Still needs to be filtered to just show the current month

--- src/components/upcoming-bills.js
+++ src/components/upcoming-bills.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import AccountCard from './account-card';
 // import { Link, Redirect } from 'react-router-dom';
 
 export class UpcomingBills extends React.Component {
+  
   render() {
+    let accountResults;
+    if (this.props.accounts){
+        accountResults= this.props.accounts.map(account=>
+            <AccountCard key={account.id}{...account}/>)
+    };
     return (
-      <section className="upcoming-bills">
-        <p>List of all upcoming bills in chronological order</p>
-      </section>
+        <div>
+            <h3>Upcoming</h3>
+            {accountResults}
+        </div>
     );
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  accounts: state.accounts.accounts
+});
 
 export default connect(mapStateToProps)(UpcomingBills);
