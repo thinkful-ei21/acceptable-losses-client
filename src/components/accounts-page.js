bb7c8d8856792642b1826b4b40f2d8fd327cbdb8
Leo Veres <leoveres@gmail.com>
2018-08-22 17:18:46 -0400

    rework next due

--- src/components/accounts-page.js
+++ src/components/accounts-page.js
@@ -7,7 +7,6 @@ import SearchBar from './search-bar';
 import { getAccount } from '../actions/accounts';
 
 export class Accounts extends React.Component {
-<<<<<<< HEAD
 
     showDetailed(id){
         this.props.dispatch(getAccount(id))
@@ -37,39 +36,10 @@ export class Accounts extends React.Component {
                 {accountResults}
             </div>
         );
-=======
-  showDetailed(id) {
-    this.props.dispatch(getAccount(id));
-  }
-
-  render() {
-    let accountResults;
-    if (this.props.accounts) {
-      accountResults = this.props.accounts.map(account => (
-        <AccountCard
-          showDetailed={id => this.showDetailed(id)}
-          key={account.id}
-          {...account}
-        />
-      ));
->>>>>>> 8fe5539705bb5d7c615547b985e7a2645ed4a1b5
     }
-    return (
-      <div className="accounts">
-        <h3>Accounts</h3>
-        <SearchBar />
-        <AccountView />
-        <p>
-          ---------------------------------------------------------------------------
-        </p>
-        {accountResults}
-      </div>
-    );
-  }
 }
 
 const mapStateToProps = state => {
-<<<<<<< HEAD
 
     return {
         alphaSort: state.accounts.alphaSort,
@@ -83,20 +53,6 @@ const mapStateToProps = state => {
 
             )
     };
-=======
-  return {
-    accounts: state.accounts.accounts.filter(
-      item =>
-        item.name.toLowerCase().includes(state.accounts.searchTerm) ||
-        item.url.toLowerCase().includes(state.accounts.searchTerm) ||
-        item.bills.find(item =>
-          item.dueDate.includes(state.accounts.searchTerm)
-        ) ||
-        item.bills.find(item => item.frequency === state.accounts.searchTerm) ||
-        item.bills.find(item => item.amount === state.accounts.searchTerm)
-    )
-  };
->>>>>>> 8fe5539705bb5d7c615547b985e7a2645ed4a1b5
 };
 
 export default requiresLogin()(connect(mapStateToProps)(Accounts));
