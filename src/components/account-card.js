b5a05a8dda923c6e67a32f12342857d716df71bf
Courtney Adams <cadams8238@gmail.com>
2018-08-22 14:34:20 -0700

    merge

--- src/components/account-card.js
+++ src/components/account-card.js
@@ -1,12 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import {connect} from 'react-redux';
 import { link } from 'fs';
 
 
 class AccountCard extends React.Component {
   // earliest(array){
-  //   if(array.length>1){ 
+  //   if(array.length>1){
   //   array.reduce(function (pre, cur) {
   //     return Date.parse(pre) > Date.parse(cur) ? cur : pre;
   //   })}
@@ -32,8 +31,8 @@ let result= this.oldest(billsList)
   //   unpaidBills = this.props.bills.filter(item=> item.isPaid === false);
   //   dueDates= unpaidBills.map(item => item =item.dueDate);
   //   earliestDate= this.earliest(dueDates);
-  //   mostRecent = unpaidBills.find(item => item.dueDate === earliestDate);   
-  // };  
+  //   mostRecent = unpaidBills.find(item => item.dueDate === earliestDate);
+  // };
   return(
     <div className="account-box">
       <h4>{this.props.name}</h4>
@@ -43,45 +42,7 @@ let result= this.oldest(billsList)
       <button onClick= {e=>this.props.showDetailed(this.props.id)}>v</button>
       <p>---------------------------------------------------------------------------</p>
     </div>
-  ) 
-=======
-import { connect } from 'react-redux';
-
-class AccountCard extends React.Component {
-  earliest(array) {
-    if (array.length > 1) {
-      array.reduce(function(pre, cur) {
-        return Date.parse(pre) > Date.parse(cur) ? cur : pre;
-      });
-    } else return array[0];
-  }
-
-  render() {
-    let mostRecent, unpaidBills, dueDates, earliestDate;
-
-    if (this.props.bills) {
-      unpaidBills = this.props.bills.filter(item => item.isPaid === false);
-      dueDates = unpaidBills.map(item => (item = item.dueDate));
-      earliestDate = this.earliest(dueDates);
-      mostRecent = unpaidBills.find(item => item.dueDate === earliestDate);
-    }
-
-    return (
-      <div className="account-box">
-        <h4>{this.props.name}</h4>
-        <p>due date: {mostRecent.dueDate} </p>
-        <p>amount due: {mostRecent.amount ? mostRecent.amount : 'TBD'} </p>
-        <button target="_blank" href={this.props.url}>
-          pay now
-        </button>
-        <button onClick={e => this.props.showDetailed(this.props.id)}>v</button>
-        <p>
-          ---------------------------------------------------------------------------
-        </p>
-      </div>
-    );
->>>>>>> 8fe5539705bb5d7c615547b985e7a2645ed4a1b5
-  }
+  )}
 }
 
 export default connect()(AccountCard);
