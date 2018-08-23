151bbcec4d8f8ba5d75ec302d2a97c22d3a6d1ae
Courtney Adams <cadams8238@gmail.com>
2018-08-23 14:17:32 -0700

    beginning to add styles to registration page

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 
 import RegistrationForm from './registration-form';
+import styles from './styles/forms.module.css';
 
 export function RegistrationPage(props) {
   if (props.loggedIn) {
     return <Redirect to="/dashboard" />;
   }
   return (
-    <div className="home">
+    <div className={styles.background}>
       <RegistrationForm />
       <Link className="register-link" to="/login">
         Already have an account? Login here!
