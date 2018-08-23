68d702b35365856ab25f3a4183831d452ffbaec4
Courtney Adams <cadams8238@gmail.com>
2018-08-23 11:03:29 -0700

    rendering dummy pie graph to summary-expenses component

--- src/components/graph-expenses.js
+++ src/components/graph-expenses.js
@@ -1,8 +1,290 @@
 import React from 'react';
+import { ResponsivePie } from '@nivo/pie'
+
 
 export default function GraphExpenses() {
 
   return (
-    <div>hello world</div>
+    <div style={{height: '500px'}}>
+      <ResponsivePie
+          data={[
+            {
+              "id": "ruby",
+              "label": "ruby",
+              "value": 7,
+              "color": "hsl(289, 70%, 50%)"
+            },
+            {
+              "id": "make",
+              "label": "make",
+              "value": 167,
+              "color": "hsl(206, 70%, 50%)"
+            },
+            {
+              "id": "haskell",
+              "label": "haskell",
+              "value": 71,
+              "color": "hsl(253, 70%, 50%)"
+            },
+            {
+              "id": "c",
+              "label": "c",
+              "value": 366,
+              "color": "hsl(173, 70%, 50%)"
+            },
+            {
+              "id": "hack",
+              "label": "hack",
+              "value": 439,
+              "color": "hsl(88, 70%, 50%)"
+            }
+          ]}
+          margin={{
+              "top": 40,
+              "right": 80,
+              "bottom": 80,
+              "left": 80
+          }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          colors="nivo"
+          colorBy="id"
+          borderWidth={1}
+          borderColor="inherit:darker(0.2)"
+          radialLabelsSkipAngle={10}
+          radialLabelsTextXOffset={6}
+          radialLabelsTextColor="#333333"
+          radialLabelsLinkOffset={0}
+          radialLabelsLinkDiagonalLength={16}
+          radialLabelsLinkHorizontalLength={24}
+          radialLabelsLinkStrokeWidth={1}
+          radialLabelsLinkColor="inherit"
+          slicesLabelsSkipAngle={10}
+          slicesLabelsTextColor="#333333"
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+          theme={{
+              "tooltip": {
+                  "container": {
+                      "fontSize": "13px"
+                  }
+              },
+              "labels": {
+                  "textColor": "#555"
+              }
+          }}
+          defs={[
+              {
+                  "id": "dots",
+                  "type": "patternDots",
+                  "background": "inherit",
+                  "color": "rgba(255, 255, 255, 0.3)",
+                  "size": 4,
+                  "padding": 1,
+                  "stagger": true
+              },
+              {
+                  "id": "lines",
+                  "type": "patternLines",
+                  "background": "inherit",
+                  "color": "rgba(255, 255, 255, 0.3)",
+                  "rotation": -45,
+                  "lineWidth": 6,
+                  "spacing": 10
+              }
+          ]}
+          fill={[
+              {
+                  "match": {
+                      "id": "ruby"
+                  },
+                  "id": "dots"
+              },
+              {
+                  "match": {
+                      "id": "c"
+                  },
+                  "id": "dots"
+              },
+              {
+                  "match": {
+                      "id": "go"
+                  },
+                  "id": "dots"
+              },
+              {
+                  "match": {
+                      "id": "python"
+                  },
+                  "id": "dots"
+              },
+              {
+                  "match": {
+                      "id": "scala"
+                  },
+                  "id": "lines"
+              },
+              {
+                  "match": {
+                      "id": "lisp"
+                  },
+                  "id": "lines"
+              },
+              {
+                  "match": {
+                      "id": "elixir"
+                  },
+                  "id": "lines"
+              },
+              {
+                  "match": {
+                      "id": "javascript"
+                  },
+                  "id": "lines"
+              }
+          ]}
+          legends={[
+              {
+                  "anchor": "bottom",
+                  "direction": "row",
+                  "translateY": 56,
+                  "itemWidth": 100,
+                  "itemHeight": 18,
+                  "symbolSize": 18,
+                  "symbolShape": "circle"
+              }
+          ]}
+      />
+    </div>
   );
 }
+
+
+
+// make sure parent container have a defined height when using responsive component,
+// otherwise height will be 0 and no chart will be rendered.
+// website examples showcase many properties, you'll often use just a few of them.
+// render((
+//     <ResponsivePie
+//         data={/* see data tab */}
+//         margin={{
+//             "top": 40,
+//             "right": 80,
+//             "bottom": 80,
+//             "left": 80
+//         }}
+//         innerRadius={0.5}
+//         padAngle={0.7}
+//         cornerRadius={3}
+//         colors="nivo"
+//         colorBy="id"
+//         borderWidth={1}
+//         borderColor="inherit:darker(0.2)"
+//         radialLabelsSkipAngle={10}
+//         radialLabelsTextXOffset={6}
+//         radialLabelsTextColor="#333333"
+//         radialLabelsLinkOffset={0}
+//         radialLabelsLinkDiagonalLength={16}
+//         radialLabelsLinkHorizontalLength={24}
+//         radialLabelsLinkStrokeWidth={1}
+//         radialLabelsLinkColor="inherit"
+//         slicesLabelsSkipAngle={10}
+//         slicesLabelsTextColor="#333333"
+//         animate={true}
+//         motionStiffness={90}
+//         motionDamping={15}
+//         theme={{
+//             "tooltip": {
+//                 "container": {
+//                     "fontSize": "13px"
+//                 }
+//             },
+//             "labels": {
+//                 "textColor": "#555"
+//             }
+//         }}
+//         defs={[
+//             {
+//                 "id": "dots",
+//                 "type": "patternDots",
+//                 "background": "inherit",
+//                 "color": "rgba(255, 255, 255, 0.3)",
+//                 "size": 4,
+//                 "padding": 1,
+//                 "stagger": true
+//             },
+//             {
+//                 "id": "lines",
+//                 "type": "patternLines",
+//                 "background": "inherit",
+//                 "color": "rgba(255, 255, 255, 0.3)",
+//                 "rotation": -45,
+//                 "lineWidth": 6,
+//                 "spacing": 10
+//             }
+//         ]}
+//         fill={[
+//             {
+//                 "match": {
+//                     "id": "ruby"
+//                 },
+//                 "id": "dots"
+//             },
+//             {
+//                 "match": {
+//                     "id": "c"
+//                 },
+//                 "id": "dots"
+//             },
+//             {
+//                 "match": {
+//                     "id": "go"
+//                 },
+//                 "id": "dots"
+//             },
+//             {
+//                 "match": {
+//                     "id": "python"
+//                 },
+//                 "id": "dots"
+//             },
+//             {
+//                 "match": {
+//                     "id": "scala"
+//                 },
+//                 "id": "lines"
+//             },
+//             {
+//                 "match": {
+//                     "id": "lisp"
+//                 },
+//                 "id": "lines"
+//             },
+//             {
+//                 "match": {
+//                     "id": "elixir"
+//                 },
+//                 "id": "lines"
+//             },
+//             {
+//                 "match": {
+//                     "id": "javascript"
+//                 },
+//                 "id": "lines"
+//             }
+//         ]}
+//         legends={[
+//             {
+//                 "anchor": "bottom",
+//                 "direction": "row",
+//                 "translateY": 56,
+//                 "itemWidth": 100,
+//                 "itemHeight": 18,
+//                 "symbolSize": 18,
+//                 "symbolShape": "circle"
+//             }
+//         ]}
+//     />
+// ), document.getElementById('chart'))
