188be70a14bbe8e3aebcd36512497d3c85fe1d78
Courtney Adams <cadams8238@gmail.com>
2018-08-27 11:54:51 -0700

    navbar is responsive, but unstyled. when window width is above 800px, the hamburger menu will disappear and all the links will show. when it's below 800px, the hamburger menu will show and toggle the menu

--- src/components/header.js
+++ src/components/header.js
@@ -4,36 +4,104 @@ import { clearAuth } from '../actions/auth';
 import { clearAuthToken } from '../local-storage';
 import { NavLink } from 'react-router-dom';
 
+import styles from './styles/navbar.module.css';
+
+
 export class HeaderBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    }
+  }
   logOut() {
     this.props.dispatch(clearAuth());
     clearAuthToken();
   }
 
+  toggleShowMenu() {
+    this.setState({
+      showMenu: !this.state.showMenu
+    })
+  }
+
   render() {
-    let header;
+    let header,
+        menu;
+
     if (this.props.loggedIn) {
       header = (
         <div>
           {/* <h1>Acceptable Losses</h1> */}
-          <div>
-            <button className="logout-button" onClick={() => this.logOut()}>
-              Log out
+          <div className={styles.mobile}>
+            <button onClick={() => this.toggleShowMenu()}>
+              <img className={styles.menu}
+                src={require('../assets/menu.svg')}
+                alt="hamburger menu icon"
+              />
             </button>
-            <NavLink role="navigation" className="link" to="/dashboard">
+            {this.state.showMenu ?
+              <React.Fragment>
+                <NavLink role="navigation" to="/dashboard">
+                  <img />
+                  Home
+                </NavLink>
+                <NavLink role="navigation" to="/accounts">
+                  <img />
+                  Accounts
+                </NavLink>
+                <NavLink role="navigation" to="/add-account">
+                  <img />
+                  Add Bill
+                </NavLink>
+                <button onClick={() => this.logOut()}>
+                  Log out
+                </button>
+              </React.Fragment>
+              :
+            ''}
+          </div>
+          <div className={styles.desktop}>
+            <NavLink role="navigation" to="/dashboard">
               Home |
             </NavLink>
-            <NavLink role="navigation" className="link" to="/accounts">
+            <NavLink role="navigation" to="/accounts">
               Accounts |
             </NavLink>
-            <NavLink role="navigation" className="link" to="/add-account">
+            <NavLink role="navigation" to="/add-account">
               Add Bill
             </NavLink>
+            <button onClick={() => this.logOut()}>
+              Log out
+            </button>
           </div>
         </div>
       );
     }
-    return <div>{header}</div>;
+
+    // if (this.state.showMenu) {
+    //   menu = (
+    //     <React.Fragment>
+    //       <NavLink role="navigation" to="/dashboard">
+    //         <img />
+    //         Home
+    //       </NavLink>
+    //       <NavLink role="navigation" to="/accounts">
+    //         <img />
+    //         Accounts
+    //       </NavLink>
+    //       <NavLink role="navigation" to="/add-account">
+    //         <img />
+    //         Add Bill
+    //       </NavLink>
+    //       <button onClick={() => this.logOut()}>
+    //         Log out
+    //       </button>
+    //     </React.Fragment>
+    //   )
+    // }
+
+    return <div className={styles.background}>{header}</div>;
   }
 }
 
